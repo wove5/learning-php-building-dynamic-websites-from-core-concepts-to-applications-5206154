1b64f8d6ae032e8dc5b22fc9816245f3c129a14c
Joe Casabona <jcasabona@gmail.com>
2025-01-30 16:30:06 +0000

    Code for 01_04

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = 'Joe';
+echo "Hello, $name \n";
+
+echo 'Hello, ' . $name . '!' . "\n";
+
+echo "Joe's nickname in high school was \"Joey Calzone\" \n";
+
+echo 'Joe\'s nickname in high school was "Joey Calzone"';
