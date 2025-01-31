ab0a39298d50f1ed16ad671ff599156043983116
Joe Casabona <jcasabona@gmail.com>
2025-01-31 22:36:21 +0000

    06_02 code

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require 'inc/variables.php';
+?>
+<html>
+
+<head>
+  <title><?php echo $title; ?></title>
+</head>
+
+<body>
+  <?php include 'templates/nav.php'; ?>
+  <h1><?php echo $title; ?></h1>
+  <p><?php echo $description; ?></p>
+  <small>By: <?php echo $author; ?></small>
+</body>
+
+</html>
