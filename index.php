1cdf43b394501f36e613418f9fd8623e4a029576
Joe Casabona <jcasabona@gmail.com>
2025-01-31 15:33:54 +0000

    03_03 code

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$a = 5;
+$b = 7;
+
+echo 'The value of $a is ' . $a++ . "\n";
+echo $a . "\n";
+
+$a = 5;
+echo 'The value of $a is ' . ++$a . "\n";
+echo $a . "\n";
+
+// Shorthand Decrementing
+echo 'The value of $b is ' . $b-- . "\n";
+echo $b . "\n";
+
+$b = 7;
+echo 'The value of $b is ' . --$b . "\n";
+echo $b;
