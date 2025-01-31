342ac73a7875c74ed69279249c3d4d127969a27d
Joe Casabona <jcasabona@gmail.com>
2025-01-31 16:53:34 +0000

    04_03 code

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
-
-/*
-do {
-  //some code
-} while ( $condition ); 
-
-while ( $condition ) {
-  // some code
-}
-*/
-
+// While loop for reference: 
 $i = 0;
 while ($i <= 10) {
   echo "$i \n";
   $i++;
 }
 
+/*
+for (starting assignment; condition; incrementor) {
+  // some code
+}
+*/
+
+for ($i = 0; $i <= 10; $i++) {
+  echo "$i \n";
+}
+
+$colors = ['red', 'green', 'blue', 'yellow'];
+
+for ($i = 0; $i < sizeof($colors); $i++) {
+  echo $colors[$i] . "\n";
+}
