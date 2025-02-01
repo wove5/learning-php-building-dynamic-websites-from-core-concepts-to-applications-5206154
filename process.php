3c43580e3416100eb3f10d267ac3ce70d28f54ed
Joe Casabona <jcasabona@gmail.com>
2025-02-01 17:30:18 +0000

    08_07 code

--- process.php
+++ process.php
@@ -1,24 +1,35 @@
 <?php
-if (!empty($_POST)) {
+if (! empty($_POST)) {
   required('Name', $_POST['name']);
-
   required('Email', $_POST['email']);
-
-  $_POST['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-
   $form_completed = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-
   required('Reason for Contact', $_POST['reason']);
   required('Message', $_POST['message']);
-}
 
-if ($form_completed ?? true) {
-  array_pop($_POST);
-  foreach ($_POST as $label => $value) {
-    if (is_array($value)) {
-      echo '<p><strong>' . ucfirst($label) . ':</strong> ' . implode(', ', $value) . '</p>';
-    } else {
-      echo '<p><strong>' . ucfirst($label) . ':</strong> ' . validate($value) . '</p>';
+
+  if ($form_completed ?? true) {
+    array_pop($_POST);
+    $name = $_POST['name'];
+    $email =
+      filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $to = "Joe Casabona<joe@casabona.org>";
+    $from = "$name<$email>";
+    $subject = "Reason for Contact: " . ucfirst($_POST['reason']);
+    $message = '';
+    foreach ($_POST as $label => $value) {
+      if (is_array($value)) {
+        $message .= ucfirst($label) . ': ' . implode(', ', $value) . "\n\n";
+      } else {
+        $message .= ucfirst($label) . ': ' . validate($value) . "\n\n";
+      }
+    }
+
+    $headers = 'From: ' . $from . " \r\n" .
+      'Reply-To: ' . $email . "\r\n" .
+      'X-Mailer: PHP/' . phpversion();
+
+    if (mail($to, $subject, $message, $headers)) {
+      echo "<h3>Your message has been sent!</h3>";
     }
   }
 }
